9e5149b48419f5dbc035770bf42b5a65ad5ed065
Yaroslav Korolev <yarehack@Yaroslavs-MacBook-Pro.local>
2022-04-16 17:53:58 +0300

    fixing problem with one array

--- wor.py
+++ wor.py
@@ -1,59 +1,47 @@
 import csv
 
-# todo
-# 1) fix shit with main array
+
 
 with open('WMT.csv') as File:
     reader = csv.reader(File)
     x = list(reader)
 
 
+    # the loop, which create a main array Date
     i = 1
-    arr_date = []
-
+    Date = []
     while i < len(x):
-        date = x[i]
-        arr_date.append(date[0])
+        dat = x[i]
+        dat_n = list(dat[0].split("-"))
+        Date.append(dat_n)
         i = i + 1 
+    print(Date)
 
-    gar = [] 
-    def good_date():
-        h = 0
-        while h < len(arr_date):
-            k = arr_date[h]
-            k_n = list(k.split("-"))
-            gar.append(k_n)
-            h = h + 1
-
-        
+    # gar = [] 
+    # def good_date():
+    #     h = 0
+    #     while h < len(arr_date):
+    #         k = arr_date[h]
+    #         k_n = list(k.split("-"))
+    #         gar.append(k_n)
+    #         h = h + 1
+    # x.pop(0)
+    # good_date()
+    # q = 0
+    # main_obj = []
+    # while q < len(x):
+    #     date = gar[q]
+    #     t = x[q]
+    #     main_dict = dict({'Year': date[0], 'Month': date[1],  1:dict({date[2]: t[1]})})
+    #     main_obj.append(main_dict)
+    #     q = q + 1
     
-        # search
-        # u = 0
-        # while u < len(gar):
-        #     new = gar[u]
-        #     if '2015' == new[0]:
-        #         print('Found')
-        #         print(new)
-        #         u = u + 1
-        #     else:
-        #         u = u + 1
-
-    x.pop(0)
-    good_date()
-    q = 0
-    main_obj = []
-    while q < len(x):
-        date = gar[q]
-        t = x[q]
-        main_dict = dict({'Year': date[0], 'Month': date[1],  1:dict({date[2]: t[1]})})
-        main_obj.append(main_dict)
-        q = q + 1
-    
-
+    #print(arr_date) # example 2022-01-24
+    #print(gar) # example ['2022', '01', '24']
     
 
 
-# good array
+
 
 
 # output of this function are 
