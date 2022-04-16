dd9deebef7773260f9b3079976a8697a44118e98
Yaroslav Korolev <yarehack@Yaroslavs-MacBook-Pro.local>
2022-04-16 19:32:03 +0300

    create_objects main function

--- wor.py
+++ wor.py
@@ -15,37 +15,29 @@ with open('WMT.csv') as File:
         dat_n = list(dat[0].split("-"))
         Date.append(dat_n)
         i = i + 1 
-    print(Date)
+    # Date[] - example: ['2022', '12', '15'] - p[0]
+    x.pop(0)
+def c_obj():
+    q = 0
+    main_obj = []
+    while q < len(x):
+        p = Date[q] # change date
+        p2 = x[q] #change data
+        main_dict = dict({'Year': p[0], 'Month': p[1],  'Day':dict({p[2]: p2[1]})})
+        main_obj.append(main_dict)
+        q = q + 1
+    print(main_obj[13])
 
-    # gar = [] 
-    # def good_date():
-    #     h = 0
-    #     while h < len(arr_date):
-    #         k = arr_date[h]
-    #         k_n = list(k.split("-"))
-    #         gar.append(k_n)
-    #         h = h + 1
-    # x.pop(0)
-    # good_date()
-    # q = 0
-    # main_obj = []
-    # while q < len(x):
-    #     date = gar[q]
-    #     t = x[q]
-    #     main_dict = dict({'Year': date[0], 'Month': date[1],  1:dict({date[2]: t[1]})})
-    #     main_obj.append(main_dict)
-    #     q = q + 1
-    
-    #print(arr_date) # example 2022-01-24
-    #print(gar) # example ['2022', '01', '24']
+c_obj()
+
     
 
 
 
 
 
-# output of this function are 
-# 1) gar - array with date arrayes
+
+
 
 # print('\n1)Open price\n 2)Close price\n 3)High price\n 4)Low Price\n 5)Volume')
 # u = int(input('What information do you want to see?\n Enter the number --> '))
